f00f955be54a8b25ab43f98fb8b0425874ce6f0c
Dmitry Kutsenko <d2emonium@gmail.com>
2018-01-14 04:45:32 +0300

    Character generator

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var router = express.Router()
 var worlds = require('../models/worlds')
+var names = require('../models/names/medieval')
+var clothing = require('../models/clothing/medieval')
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -18,6 +20,61 @@ router.get('/worlds', function (req, res) {
 
   res.send({
     'worlds': generated
+  })
+})
+
+router.get('/names', function (req, res) {
+  var count = req.query.count
+  if (!count) { count = 1 }
+
+  var sex = req.query.sex
+
+  var generated = []
+  for (var i=0; i < count; i++) {
+    generated.push(names.generate(sex))
+  }
+
+  res.send({
+    'names': generated
+  })
+})
+
+router.get('/clothing', function (req, res) {
+  var count = req.query.count
+  if (!count) { count = 1 }
+
+  var sex = req.query.sex
+
+  var generated = []
+  for (var i=0; i < count; i++) {
+    generated.push(clothing.generate(sex))
+  }
+
+  res.send({
+    'count': count,
+    'sex': sex,
+    'clothing': generated
+  })
+})
+
+router.get('/characters', function (req, res) {
+  var count = req.query.count
+  if (!count) { count = 1 }
+
+  var sex = req.query.sex
+
+  var generated = []
+  for (var i=0; i < count; i++) {
+    generated.push({
+      'count': count,
+      'sex': sex,
+      'name': names.generate(sex),
+      'clothing': clothing.generate(sex),
+    })
+  }
+
+  res.send({
+    'characters': generated
   })
 })
 
